e0338ef96ef2c470da76fb95c499fccd82a757d5
worlddailyreport <stickerjunkies21@gmail.com>
2025-02-09 11:41:46 -0700

    Update create.js

--- netlify/functions/create.js
+++ netlify/functions/create.js
@@ -1,3 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const dbFilePath = path.join(__dirname, "data.json");
+
+// Load existing stored data
+function loadDatabase() {
+    if (fs.existsSync(dbFilePath)) {
+        return JSON.parse(fs.readFileSync(dbFilePath, "utf8"));
+    }
+    return {};
+}
+
+// Save new data to file
+function saveDatabase(data) {
+    fs.writeFileSync(dbFilePath, JSON.stringify(data, null, 2));
+}
+
 exports.handler = async (event) => {
     try {
         const { headline, imageUrl } = JSON.parse(event.body);
@@ -9,15 +27,19 @@ exports.handler = async (event) => {
             };
         }
 
-        // Create a URL-friendly slug
         const slug = headline.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)/g, "");
         const shortUrl = `https://worlddailyreport.com/article/${slug}`;
+
+        // Load current database
+        const db = loadDatabase();
+        db[slug] = { headline, imageUrl };
+
+        // Save updated database
+        saveDatabase(db);
 
         return {
             statusCode: 200,
             body: JSON.stringify({ shortUrl }),
         };
     } catch (err) {
-        return { statusCode: 500, body: JSON.stringify({ error: "Server error" }) };
-    }
-};
+        retu
