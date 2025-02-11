b5fb0af43167b65db780ea489d2fc67f6134cd84
worlddailyreport <stickerjunkies21@gmail.com>
2025-02-10 19:04:59 -0700

    Update get-article.js

--- netlify/functions/get-article.js
+++ netlify/functions/get-article.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const dataFilePath = path.join(__dirname, "data.json");
+const dataFilePath = path.join("/tmp", "data.json");  // ✅ Matches create.js
 
 // ✅ Load stored articles
 function loadArticles() {
